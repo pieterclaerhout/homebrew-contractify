ad0e340b29396632c634f653c586aaff7338a9e5
Pieter Claerhout <pieter@yellowduck.be>
2023-05-30 11:46:26 +0200

    Bumped the version

--- Formula/contractify-cli.rb
+++ Formula/contractify-cli.rb
@@ -1,9 +1,9 @@
 class ContractifyCli < Formula
   desc "CLI interface for Contractify"
   homepage "https://contractify.io"
-  url "https://contractify-builds.fra1.digitaloceanspaces.com/contractify-cli/contractify-cli-darwin-arm64.tar.gz"
-  version "2023-05-26T18.42.37"
-  sha256 "d60dfcd3acf12ebaf0669e1b801fc806a5159fc2a502f5f109fbe2c0943dfbda"
+  url "https://contractify-builds.fra1.digitaloceanspaces.com/contractify/contractify-cli/ffe78a852b95358f2d89fd2f0f111f742244d8bf/contractify-cli-darwin-arm64.tar.gz"
+  version "6e7e4fc"
+  sha256 "803ebd06e2396a90c20a3cd9ae33af6413d5afe239d2e6cedd0a377c72c990d6"
   license "MIT"
 
   def install
