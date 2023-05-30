4fb806b95534e64597c9bdd0bfc2e6b797e327a3
Pieter Claerhout <pieter@yellowduck.be>
2023-05-30 13:09:39 +0200

    Changed the version name

--- Formula/contractify-cli.rb
+++ Formula/contractify-cli.rb
@@ -2,7 +2,7 @@ class ContractifyCli < Formula
   desc "CLI interface for Contractify"
   homepage "https://contractify.io"
   url "https://contractify-builds.fra1.digitaloceanspaces.com/contractify/contractify-cli/ffe78a852b95358f2d89fd2f0f111f742244d8bf/contractify-cli-darwin-arm64.tar.gz"
-  version "6e7e4fc"
+  version "2023-05-30T09.41.27"
   sha256 "803ebd06e2396a90c20a3cd9ae33af6413d5afe239d2e6cedd0a377c72c990d6"
   license "MIT"
 
